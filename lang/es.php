e5fe8d582ef09bad231ade768e12b039a103d3bb
Israel7KerneL <israelaguilarcruz@gmail.com>
2021-04-28 11:02:40 -0500

    Translate Pagina PHP Multilenguaje Traducción Español Ingles Funcional.

---
+++ lang/es.php
@@ -0,0 +1,13 @@
+<?php
+// Definimos todas las palabras en ingles un array
+$lang = array(
+  "titulo" => "Titulo",
+  "logo" => "Logo",
+  "cambiar_idioma" => "Cambiar Idioma",
+  "cambiar" => "Cambiar",
+  "opcion_1" => "Seleccionar",
+  "opcion_2" => "Español",
+  "opcion_3" => "Ingles",
+  "descripcion" => "Hola, Este Es Un Titulo En Español",
+);
+?>
